972e2a939f91440acee0d59a863dc68371b9f777
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-11-20 04:26:56 +0500

    Solved with brute force

---
+++ 10_two_sum_II.ts
@@ -0,0 +1,35 @@
+function twoSum (numbers: number[], target: number): number[] {
+  let left = 0
+  let right = numbers.length - 1
+
+  while (left < right) {
+    let result = numbers[left] + numbers[right]
+
+    if (result === target) {
+      return [left + 1, right + 1]
+    }
+
+    if (result < target) {
+      left += 1
+    }
+
+    if (result > target) {
+      right -= 1
+    }
+  }
+
+  return []
+}
+
+function twoSum2 (numbers: number[], target: number): number[] {
+  for (let i = 0; i <= numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1]
+      }
+    }
+  }
+  return []
+}
+
+console.log(twoSum2([2,7,11,15], 9))
