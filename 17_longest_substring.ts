a72f6032e4d1ebf1ddbcde26133887c363c2c7c1
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-12-15 22:56:25 +0500

    Added solution of longest substring

---
+++ 17_longest_substring.ts
@@ -0,0 +1,19 @@
+function longestSubstring (s: string): number {
+  let max_length = 0
+  let seen: { [key: string]: number } = {}
+  let start = 0
+
+  for (let i = 0; i < s.length; i++) {
+    seen[s[i]] = (seen[s[i]] || 0) + 1
+    while (seen[s[i]] > 1) {
+      seen[s[start]]--
+      start++
+    }
+
+    max_length = Math.max(max_length, i - start + 1)
+  }
+
+  return max_length
+}
+
+longestSubstring('abcabcbb')
